c8d74c37e1a2fbcd0a53469f9756196d99223fb6
CharlesEduardoSCBR <charles.eduardo.sc.br@gmail.com>
2019-10-13 16:38:54 -0300

    5.08 -  Adicionando mais um componente (refatorando a tag imagem)

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
     <ul class="lista-fotos">
       <li class="lista-fotos-item" v-for="foto of fotosComFiltro" v-bind:key="foto.id">
         <meu-painel :titulo="foto.titulo">
-          <img class="responsive-image" :src="foto.url" :alt = "foto.titulo" />
+          <imagem-responsiva :url="foto.url" :titulo="foto.titulo" />
         </meu-painel>
       </li>
     </ul>    
@@ -17,11 +17,13 @@
 
 <script>
 import Painel from './components/shared/painel/Painel.vue'
+import ImagemResponsiva from './components/shared/imagem-responsiva/ImagemResponsiva.vue'
 
 export default {
 
   components: {
-    'meu-painel' : Painel
+    'meu-painel' : Painel,
+    'imagem-responsiva': ImagemResponsiva
   },
 
   data(){
@@ -75,10 +77,6 @@ export default {
  .lista-fotos, .lista-fotos-item {
    display: inline-block;
  }
-
-  .responsive-image {
-    width: 100%;
-  }
 
   .filtro {
     width: 100%;
---
+++ src/components/shared/imagem-responsiva/ImagemResponsiva.vue
@@ -0,0 +1,18 @@
+<template>
+  <img class="responsive-image" :src="url" :alt = "titulo" />
+</template>
+
+<script>
+export default {
+
+    props: ['url', 'titulo']
+}
+</script>
+
+<style scope>
+
+  .responsive-image {
+    width: 100%;
+  }
+
+</style>
