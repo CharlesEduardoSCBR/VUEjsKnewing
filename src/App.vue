d08aee3b816fe0880479f7fc557ee25f409690c0
CharlesEduardoSCBR <charles.eduardo.sc.br@gmail.com>
2019-10-13 13:14:44 -0300

    2 - Inserind o ng-for em uma lista

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <h1>{{ titulo }}</h1>
-    <img :src="foto.url" :alt = "foto.titulo" />
+    <ul>
+      <li  v-for="foto of fotos">
+        <img :src="foto.url" :alt = "foto.titulo" />
+      </li>
+    </ul>    
   </div>
 </template>
 
@@ -11,10 +15,16 @@ export default {
   data(){
     return {
       titulo: 'VUEjsPIC',
-      foto: {
-        url: 'https://previews.123rf.com/images/peshkova/peshkova1707/peshkova170700975/83909100-abstract-glowing-polygonal-tech-lamp-on-dark-blue-background-innovative-idea-concept-3d-rendering.jpg',
-        titulo: 'lamp'
-      }
+      fotos:[ 
+        {
+          url: 'https://previews.123rf.com/images/peshkova/peshkova1707/peshkova170700975/83909100-abstract-glowing-polygonal-tech-lamp-on-dark-blue-background-innovative-idea-concept-3d-rendering.jpg',
+          titulo: 'lamp'
+        },
+        {
+          url: 'http://1.bp.blogspot.com/-Oj0HzE1zpsk/VKBvu6GLsdI/AAAAAAABARs/7z44skyA6og/s1600/High-tech-Creative-Lamp.jpg',
+          titulo: 'brain'
+        }
+      ]
     }
   }
 }
