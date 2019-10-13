dee3178708a2bf4aab3e6bcca3fbc14dc0f50ae5
CharlesEduardoSCBR <charles.eduardo.sc.br@gmail.com>
2019-10-13 14:14:34 -0300

    3.7 - $http e promisses

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>{{ titulo }}</h1>
     <ul>
-      <li  v-for="foto of fotos" v-bind:key="foto.id">
+      <li v-for="foto of fotos" v-bind:key="foto.id">
         <img :src="foto.url" :alt = "foto.titulo" />
       </li>
     </ul>    
@@ -15,17 +15,16 @@ export default {
   data(){
     return {
       titulo: 'VUEjsPIC',
-      fotos:[ 
-        {
-          url: 'https://previews.123rf.com/images/peshkova/peshkova1707/peshkova170700975/83909100-abstract-glowing-polygonal-tech-lamp-on-dark-blue-background-innovative-idea-concept-3d-rendering.jpg',
-          titulo: 'lamp'
-        },
-        {
-          url: 'http://1.bp.blogspot.com/-Oj0HzE1zpsk/VKBvu6GLsdI/AAAAAAABARs/7z44skyA6og/s1600/High-tech-Creative-Lamp.jpg',
-          titulo: 'brain'
-        }
-      ]
+      fotos:[]
     }
+  },
+
+  created(){
+    let promise = this.$http.get('http://localhost:3000/v1/fotos');
+
+    promise.then(res => 
+      res.json().then(fotos => this.fotos = fotos)
+    );
   }
 }
 </script>
