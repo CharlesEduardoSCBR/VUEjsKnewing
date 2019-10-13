bf2ba42fdc3590127c2858346d2cc353047a3bfc
CharlesEduardoSCBR <charles.eduardo.sc.br@gmail.com>
2019-10-13 14:51:01 -0300

    4.2 - Continuação estilizando o componente

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,12 @@
     
     <ul class="lista-fotos">
       <li class="lista-fotos-item" v-for="foto of fotos" v-bind:key="foto.id">
-        <img :src="foto.url" :alt = "foto.titulo" />
+        <div class="painel">
+          <h2 class="painel-titulo">{{ foto.titulo }}</h2>
+          <div class="painel-conteudo">
+            <img class="responsive-image" :src="foto.url" :alt = "foto.titulo" />
+          </div>
+        </div>
       </li>
     </ul>    
   </div>
@@ -30,22 +35,48 @@ export default {
 </script>
 
 <style>
- .corpo{
+ .corpo {
    font-family: Helvetica, sans-serif;
    width: 96%;
    margin: 0 auto;
  }
 
- .centralizado{
+ .centralizado {
    text-align: center;
  }
 
- .lista-fotos{
+ .lista-fotos {
    list-style: none;
  }
 
- .lista-fotos, .lista-fotos-item{
+ .lista-fotos, .lista-fotos-item {
    display: inline-block;
  }
+
+  .responsive-image {
+    width: 100%;
+  }
+
+  /* estilo do painel */
+  .painel {
+    padding: 0 auto;
+    border: solid 2px grey;
+    display: inline-block;
+    margin:  5px;
+    box-shadow: 5px 5px 10px grey;
+    width:  200px;
+    height: 100%;
+    vertical-align: top;
+    text-align: center;
+  }
+
+  .painel .painel-titulo {
+    text-align: center;
+    border: solid 2px;
+    background: lightblue;
+    margin: 0 0 15px 0;
+    padding: 10px;
+    text-transform:  uppercase;
+  }
 
 </style>
