11ba628b555d37104c78e49dbdbca98fe7614545
CharlesEduardoSCBR <charles.eduardo.sc.br@gmail.com>
2019-10-13 18:10:16 -0300

    06.05 - Tirando a # do path do navegador

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ Vue.config.productionTip = false
 Vue.use(VueResource);
 Vue.use(VueRouter);
 
-const router = new VueRouter({ routes });
+const router = new VueRouter({ routes, mode: "history" });
 
 new Vue({
   render: h => h(App),
